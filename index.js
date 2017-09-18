7b8b775738ef55bb25ef0f4da9604dff2b28366d
Michael <mikeks81@gmail.com>
2017-09-18 02:01:57 -0400

    implements sign in/out, session cookies

--- index.js
+++ index.js
@@ -1,20 +1,27 @@
 const express = require('express')
 const mongoose = require('mongoose')
+const cookieSession = require('cookie-session')
+const passport = require('passport')
 const keys = require('./config/keys')
+require('./models/User')
 require('./services/passport')
-require('./routes/authRoutes')
 
-const db = mongoose.connect(keys.mongodbDevURI)
+mongoose.connect(keys.mongodbDevURI)
 
 const app = express()
+app.use(
+  cookieSession({
+    // 30 days in milliseconds
+    maxAge: 30 * 24 * 60 * 60 * 1000,
+    keys: [keys.cookieKey]
+  })
+)
+app.use(passport.initialize())
+app.use(passport.session())
 
 // authroutes returns a function and we call it and poss app value into thtat function
 require('./routes/authRoutes')(app)
 
-app.use((req, res, next) => {
-  req.db = db
-  next()
-})
-
 const PORT = process.env.PORT || 5000
+
 app.listen(PORT)
--- services/passport.js
+++ services/passport.js
@@ -1,6 +1,20 @@
 const passport = require('passport')
 const GoogleStrategy = require('passport-google-oauth20').Strategy
+const mongoose = require('mongoose')
+// oath variables, mongo url
 const keys = require('../config/keys')
+const User = mongoose.model('users')
+
+// the "user" passed to the serialize function is the value of the done callback in the GoogleStrategy done callback -- confusing - it's a passport thing
+passport.serializeUser((user, done) => {
+  done(null, user.id)
+})
+
+passport.deserializeUser((id, done) => {
+  User.findById(id).then(user => {
+    done(null, user)
+  })
+})
 
 passport.use(
   new GoogleStrategy(
@@ -10,9 +24,17 @@ passport.use(
       callbackURL: '/auth/google/callback'
     },
     (accessToken, refreshToken, profile, done) => {
-      console.log('access toke ', accessToken)
-      console.log('refresh token ', refreshToken)
-      console.log('profile ', profile)
+      // persisint to the db is still an asych call so you must use promises beofore calling done
+      User.findOne({ googleId: profile.id }).then(existingUser => {
+        if (existingUser) {
+          done(null, existingUser)
+        } else {
+          // persisint to the db is still an asych call so you must use promises beofore calling done
+          new User({ googleId: profile.id })
+            .save()
+            .then(user => done(null, user))
+        }
+      })
     }
   )
 )
