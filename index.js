32fec0563c685abd2f2422d3af4840efd9944dc2
Michael <mikeks81@gmail.com>
2017-09-17 23:08:07 -0400

    hopefully connected mongo correctly

--- index.js
+++ index.js
@@ -1,8 +1,19 @@
 const express = require('express')
+const mongoose = require('mongoose')
+const keys = require('./config/keys')
+require('./services/passport')
+require('./routes/authRoutes')
+
+const db = mongoose.connect(keys.mongodbDevURI)
+
 const app = express()
 
-app.get('/', (req, res) => {
-  res.send({ hi: 'there' })
+// authroutes returns a function and we call it and poss app value into thtat function
+require('./routes/authRoutes')(app)
+
+app.use((req, res, next) => {
+  req.db = db
+  next()
 })
 
 const PORT = process.env.PORT || 5000
