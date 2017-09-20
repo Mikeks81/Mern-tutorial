63671504ce9c9336f8dcee5e04658086cd6d9f1b
Michael <mikeks81@gmail.com>
2017-09-19 23:48:23 -0400

    yaaaaay no sleep

--- index.js
+++ index.js
@@ -23,12 +23,15 @@ app.use(passport.session())
 
 // authroutes returns a function and we call it and poss app value into thtat function
 require('./routes/authRoutes')(app)
-
-let httpsOptions = {
-  key: fs.readFileSync('./key.pem'),
-  cert: fs.readFileSync('./cert.pem')
+let httpsOptions = {}
+if (process.env.NODE_ENV === 'develop') {
+  httpsOptions = {
+    key: fs.readFileSync('./key.pem'),
+    cert: fs.readFileSync('./cert.pem')
+  }
+} else {
+  httpsOptions = {}
 }
-if (process.env.NODE_ENV === 'production') httpsOptions = {}
 
 const PORT = process.env.PORT || 5000
 
