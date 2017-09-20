d147e5f19b4a05da3da9c088e0c233ed12ff8d62
Michael <mikeks81@gmail.com>
2017-09-19 23:33:48 -0400

    auth flow - prod vs dev

---
+++ config/prod.js
@@ -0,0 +1,9 @@
+module.exports = {
+  googleClientID: process.env.GOOGLE_CLIENT_ID,
+  googleClientSecret: process.env.GOOGLE_CLIENT_SECRET,
+  bungieClientId: process.env.BUNGIE_CLIENT_ID,
+  bungieSecret: process.env.BUNGIE_SECRET,
+  bungieApiKey: process.env.BUNGIE_API_KEY,
+  mongoURI: process.env.MONGO_URI,
+  cookieKey: process.env.COOKIE_KEY
+}
--- index.js
+++ index.js
@@ -8,7 +8,7 @@ const keys = require('./config/keys')
 require('./models/User')
 require('./services/passport')
 
-mongoose.connect(keys.mongodbDevURI)
+mongoose.connect(keys.mongoURI)
 
 const app = express()
 app.use(
