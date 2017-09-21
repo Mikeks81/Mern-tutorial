43a155df2b6f5b8c077b2a0ccc24ce320d7eb16f
Michael <mikeks81@gmail.com>
2017-09-20 23:55:34 -0400

    configuring react to talk to server

--- client/src/App.js
+++ client/src/App.js
@@ -13,6 +13,9 @@ class App extends Component {
         <p className="App-intro">
           To get started, edit <code>src/App.js</code> and save to reload.
         </p>
+        <a href="/auth/google">Sign in with Google</a>
+        <br />
+        <a href="/auth/bungie">Sign in with Bungie</a>
       </div>
     )
   }
