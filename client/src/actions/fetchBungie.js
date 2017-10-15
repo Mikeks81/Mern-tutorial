76d32c73bf35a5d6f4162a58c060ba1650e1fe9b
Michael <mikeks81@gmail.com>
2017-10-15 03:03:15 -0400

    wiring up the client and server to lookup group data

--- client/src/actions/fetchBungie.js
+++ client/src/actions/fetchBungie.js
@@ -1,5 +1,5 @@
 import axios from 'axios'
-import { FETCH_PLAYER } from './types'
+import { FETCH_PLAYER, FETCH_GROUP } from './types'
 
 export const fetchPlayer = playerName => async dispatch => {
   const res = await axios.get(`/api/player`, {
@@ -22,4 +22,11 @@ export const fetchGearDetails = (
   typeof callback === 'function' && callback(res)
 
   return res
+}
+
+export const fetchUserGroup = playerName => async dispatch => {
+  console.log('fetching group', playerName)
+  const res = await axios.get(`/api/get_user_group/${playerName}`)
+
+  dispatch({ type: FETCH_GROUP, payload: res.data })
 }
--- client/src/reducers/bungieReducer.js
+++ client/src/reducers/bungieReducer.js
@@ -1,8 +1,10 @@
-import { FETCH_PLAYER } from '../actions/types'
+import { FETCH_PLAYER, FETCH_GROUP } from '../actions/types'
 
 export default function(state = null, action) {
   switch (action.type) {
     case FETCH_PLAYER:
+      return { ...state, ...action.payload }
+    case FETCH_GROUP:
       return { ...state, ...action.payload }
     default:
       return state
