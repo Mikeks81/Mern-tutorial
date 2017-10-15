7eb738ca6abf3c8cf645a8b42709d18bd8541a82
Michael <mikeks81@gmail.com>
2017-10-14 22:37:24 -0400

    adds carousel view for every profile

--- client/src/components/bungie/ProfileContainer.js
+++ client/src/components/bungie/ProfileContainer.js
@@ -3,11 +3,60 @@ import { connect } from 'react-redux'
 import * as actions from '../../actions/fetchBungie'
 import Profile from './profile/Banner'
 import Gear from './profile/Gear'
+import { TweenLite, Power3 } from 'gsap'
+import './ProfileContainer.css'
 
 class ProfileContainer extends Component {
+  constructor() {
+    super()
+    this.isSlide = 0
+  }
   componentDidMount() {
     console.log(this.props, actions)
     this.props.fetchPlayer('DirtiSausage')
+  }
+
+  navLeft() {
+    if (this.isSlide <= 0) return
+    const { profileCarousel } = this.refs
+    TweenLite.to(profileCarousel, 0.5, {
+      xPercent: '+=33.33',
+      ease: Power3.easeIn,
+      onComplete: () => {
+        this.isSlide -= 1
+      }
+    })
+  }
+
+  navRight() {
+    if (this.isSlide >= 2) return
+    const { profileCarousel } = this.refs
+    TweenLite.to(profileCarousel, 0.5, {
+      xPercent: '-=33.33',
+      ease: Power3.easeIn,
+      onComplete: () => {
+        this.isSlide += 1
+      }
+    })
+  }
+
+  profileNav() {
+    return (
+      <div className="profile-nav">
+        <i
+          className="small material-icons left"
+          onClick={this.navLeft.bind(this)}
+        >
+          chevron_left
+        </i>
+        <i
+          className="small material-icons right"
+          onClick={this.navRight.bind(this)}
+        >
+          chevron_right
+        </i>
+      </div>
+    )
   }
 
   renderProfiles() {
@@ -24,7 +73,7 @@ class ProfileContainer extends Component {
           characterId: profile.characterId
         }
         return (
-          <div key={i}>
+          <div className="profile-content col s4" key={i}>
             <Profile {...profile} />
             <Gear
               {...gearProps}
@@ -38,9 +87,11 @@ class ProfileContainer extends Component {
 
   render() {
     return (
-      <div>
-        <p>asdfasd</p>
-        {this.props.bungieProfile && this.renderProfiles()}
+      <div className="profile-container">
+        {this.profileNav()}
+        <div className="profile-carousel row" ref="profileCarousel">
+          {this.props.bungieProfile && this.renderProfiles()}
+        </div>
       </div>
     )
   }
